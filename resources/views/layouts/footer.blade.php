a665bc1df3eb39202f44c02d9e4ef7980a9ca81b
bothimariano <92998948+marinobo@users.noreply.github.com>
2023-11-24 16:51:56 -0600

    v2footer24/11/2023

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,84 +1,172 @@
-<html>
-    <head>
-        <style>
-        .footer
-        {
-            background-color: #691B32;
-            padding: 10px;
-            text-align: center;
-        }
-        .footer img
-        {
-            width: 100px;
-            height: auto;
-        }
-        .footer p
-        {
-            color:#fff;
-            margin: 0;
-        }
-        .footer .contacto
-        {
-            font-size: 12px;
-            color: #fff;
-        }
-        /* Nuevas reglas CSS */
-        .footer .row
-        {
-            display: flex;
-            justify-content: space-between;
-        }
-        .footer ul
-        {
-            display: inline-block;
-            list-style-type: none;
-            padding: 0;
-        }
-        .footer li
-        {
-            margin: 5px 0;
-        }
-        .footer a
-        {
-            text-decoration: none;
-            color: #fff;
-        }
-        .footer a:hover {
-            color: #fff;
-        }
-        </style>
-        <!--FONT AWESOME-->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <!--GOOGLE FONTS-->
-        <link rel="shortcut icon" type="image/x-icon" href="https://cdn.hidalgo.gob.mx/logo.png">
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Play&display=swap" rel="stylesheet"> 
-    </head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <title>Document</title>
+  </head>
+  <style>
+  @import "https://cdn.hidalgo.gob.mx/transicion/colors.css";
+  .fa-brands{
+    font-family: "Font Awesome 5 Brands" !important;
+  }
+  .main-footer{
+    background-color:var(--wine);
+    min-height:56px;
+    color:white;
+    padding:20px 0 20px;
+    text-align:left;
+    position: relative;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+  .main-footer .first-row-footer{  }
+  .main-footer .footer-logo-tels{
+    display: flex;
+    justify-content: space-between;
+  }
+  .main-footer .footer-logo{
+    margin-left: 50px;
+    display: flex;
+  }
+  .main-footer .footer-logo img{
+    width: 100px;
+    height: auto;
+    align-self: center;
+  }
+  .tels-footer{
+    display: flex;
+  }
+  .tels-footer p{
+    color: #fff;;
+  }
+  .tels-footer div{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px 20px;
+  }
+  .contacto h6:first-child{
+    margin-bottom: 20px;
+  }
+  .contacto h6:nth-child(2){
+    margin-bottom: 20px;
+  }
+  .main-footer hr{
+    border-top: 1px solid var(--gold);
+  }
+  .main-footer .second-row-footer p{
+    padding-top: 10px;
+    color: #fff;
+  }
+  .main-footer .social-media{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .main-footer .social-media a{
+    color: #fff;
+    text-decoration: none !important;
+  }
+  .main-footer .social-media i{
+    font-size: 30px;
+    margin: 0 20px;
+  }
+  @media (max-width: 992px){
+    .main-footer .footer-logo{
+      margin-left: 0px;
+    }
+  }
+  @media(max-width: 768px){
+    .main-footer .footer-logo-tels{
+      flex-direction: column;
+      align-items: center;
+    }
+    .main-footer .tels-footer div{
+      text-align: center;
+      margin-top: 50px;
+    }
+  }
+  @media (max-width:  576px){
+    .card-landing{
+      margin-top: 50px;
+    }
+    .cont-noticias-cards{
+      min-height: 500px;
+    }
+    .bann-programas-bienestar{
+      margin-top: 100px;
+    }
+    .bann-desk{
+      display: none;
+    }
+    .bann-mob{
+      display: block;
+    }
+    .cont-noticias-cards{
+      margin-top: 50px;
+    }
+    .main-footer .footer-logo-tels{
+      flex-direction: column;
+    }
+    .main-footer .tels-footer{
+      flex-direction: column;
+    }
+    .main-footer .tels-footer div{
+      margin-top: 10px;
+    }
+    }
+    </style>
     <body>
-        <footer>
-            <div class="footer">
-                <div class="row">
-                    <img src="https://cdn.hidalgo.gob.mx/gobierno/images/logos/logo_gob.png" alt="Logo de la empresa">
-                    <div>
-                        <a href="#"><i class="fa fa-facebook"></i></a>
-                        <a href="#"><i class="fa fa-whatsapp"></i></a>
-                    </div>
-                    <div class="list-container">
-                        <ul>
-                            <li><a href="https://gobierno.hidalgo.gob.mx/AvisoPrivacidad" target="_blank">Aviso de privacidad</a></li>
-                            <li><p>Contacto: citnova@hidalgo.gob.mx</p></li>
-                            <li><p>Teléfono: 771 657 5103 y 771 657 5104</p></li>
-                            <li><p>Boulevard Circuito la Concepción #3</p></li>
-                            <li><p>San Agustin Tlaxiaca, Hidalgo, México</p></li>
-                            <li><a href="https://drive.google.com/file/d/1vOWKk9IierD6XnpzIBiv-lQnhbbv9o1z/view?usp=sharing" target="_blank">Aviso de privacidad CITNOVA</a></li>
-                        </ul>
-                    </div>
+      <div class="main-footer"> 
+        <div class="container-fluid">
+          <div class="row first-row-footer">
+            <div class="col-md-9 col-12">
+              <div class="footer-logo-tels">
+                <div class="footer-logo">
+                  <img alt="logotipo gob.mx" src="https://cdn.hidalgo.gob.mx/transicion/logo_hgo_2019.png" />
                 </div>
-                <br><br>
-                <div class="row">
-                    <p>Copyright © <?php echo date("Y");?> Todos los derechos reservados por Hidalgo de la Transformación</p>
+                <div class="col-sm-3 col-12">
+                  <div class="social-media">
+                    <a href="https://www.facebook.com/gobhidalgo" target="_blanc">
+                      <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+                    <a href="https://twitter.com/gobiernohidalgo" target="_blanc">
+                      <i class="fa-brands fa-twitter"></i>
+                    </a>
+                  </div>
                 </div>
+              </div>
             </div>
-        </footer>
+            <div class="col-md-3 col-12">
+              <div class="contacto">
+                <h6><a href="https://gobierno.hidalgo.gob.mx/AvisoPrivacidad">Aviso de privacidad</a></h6>
+                <h6>Contacto: citnova@hidalgo.gob.mx</h6>
+                <h6>Teléfonos: 771 657 5103 y 771 657 5104</h6>
+                <p>Boulevard Circuito la Concepción #3</p>
+                <p>San Agustín Tlaxiaca, Hidalgo, México</p>
+                <h6>
+                  <a href="https://drive.google.com/file/d/1vOWKk9IierD6XnpzIBiv-lQnhbbv9o1z/view?usp=sharing" target="_blank">Aviso de privacidad CITNOVA</a>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <hr/>
+          <div class="row second-row-footer">
+            <div class="col-sm-9 col-12">
+              <p>Copyrights © 
+                <script type = "text/javascript">
+                let date =  new Date().getFullYear();
+                document.write( date ); 
+                </script> 
+                Todos los derechos reservados por Hidalgo de la Transformación
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </body>
 </html>
