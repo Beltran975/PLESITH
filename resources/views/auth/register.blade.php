a0b832c989abb4918ed8e38c16c91300b75e814c
Barmont1341 <131329899+Barmont1341@users.noreply.github.com>
2023-12-04 11:29:15 -0600

    v4_registro_4_12_2023

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,29 +3,29 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('asset/register.css') }}">     
+    <link rel="stylesheet" href="{{ asset('asset/register.css') }}">    
+    <link rel="shortcut icon" type="image/x-icon" href="https://cdn.hidalgo.gob.mx/logo.png"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" integrity="sha384-rbs5oRmyz9BDjzS9nEIEUnzM1qDe5ICorxF9uF8g5SeFOByuB+8Z3Gk5Sck/GJvI" crossorigin="anonymous">
-    <title>Registro</title>
+    <title>Registro | PLESITH</title>
   </head>
   <body>
     @include('layouts/headregob')
     @include('layouts/header')
-    <br><br><br><br>
-    
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div >
-            <div class="card-header">{{ __('Registro de usuario') }}</div>
-            
-            <div>
-              <form method="POST" action="{{ route('register') }}">
+    <main class="main">
+    <div class="content-registro form-group">
+            <div class="row ml-5">
+              <h3>{{ __('Registro de usuario') }}</h3>
+            </div>
+            <hr>
+             <form method="POST" action="{{ route('register') }}">
                 @csrf
                 
-                <div class="row mb-3">
-                  <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Nombre completo *') }}</label>
+                <div class="row justify-content-center mb-3">
+                  <div class="col-md-4">
+                    <label class="control-label" for="name" >{{ __('Nombre completo *') }}</label>
+                  </div>
                   <div class="col-md-6">
-                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                  <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
                     @error('name')
                     <span class="invalid-feedback" role="alert">
                       <strong>{{ $message }}</strong>
@@ -34,18 +34,22 @@
                   </div>
                 </div>
                 
-                <div class="row mb-3">
-                  <label for="curp" class="col-md-4 col-form-label text.md-end">{{ __('CURP *')}}</label>
+                <div class="row justify-content-center mb-3">
+                  <div class="col-md-4">
+                    <label class="control-label" for="curp" class="">{{ __('CURP *')}}</label>
+                  </div>
                   <div class="col-md-6">
                     <input id="curp" type="text" class="form-control" name="curp" required><br>
-                    <input id="archivoCurp" type="file" class="form-control" name="archivoCurp" required>
+                    <input class="form-control id="archivoCurp" type="file"  name="archivoCurp" required>
                   </div>
                 </div>
                 
-                <div class="row mb-3">
-                  <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Correo *') }}</label>
+                <div class="row justify-content-center mb-3">
+                  <div class="col-md-4">
+                    <label class="control-label" for="email" class="">{{ __('Correo *') }}</label>
+                  </div>
                   <div class="col-md-6">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                  <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
                     @error('email')
                     <span class="invalid-feedback" role="alert">
                       <strong>{{ $message }}</strong>
@@ -54,10 +58,12 @@
                   </div>
                 </div>
                 
-                <div class="row mb-3">
-                  <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña *') }}</label>
+                <div class="row justify-content-center mb-3">
+                  <div class="col-md-4">
+                    <label class="control-label" for="password" >{{ __('Contraseña *') }}</label>
+                  </div>
                   <div class="col-md-6">
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                  <input class="form-control @error('password') is-invalid @enderror" id="password" type="password" name="password" required autocomplete="new-password">
                     @error('password')
                     <span class="invalid-feedback" role="alert">
                       <strong>{{ $message }}</strong>
@@ -66,17 +72,21 @@
                   </div>
                 </div>
                 
-                <div class="row mb-3">
-                  <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirmar contraseña *') }}</label>
+                <div class="row justify-content-center mb-3">
+                  <div class="col-md-4">
+                    <label class="control-label" for="password-confirm">{{ __('Confirmar contraseña *') }}</label>
+                  </div>
                   <div class="col-md-6">
-                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                  <input class="form-control" id="password-confirm" type="password"  name="password_confirmation" required autocomplete="new-password">
                   </div>
                 </div>
                 
-                <div class="row mb-3">
-                  <label for="institucion" class="col-md-4 col-form-label text-md-end">{{ __('Institución a la que pertenece *')}}</label>
+                <div class="row justify-content-center mb-3">
+                  <div class="col-md-4">
+                    <label for="institucion" class="control-label">{{ __('Institución a la que pertenece *')}}</label>
+                  </div>
                   <div class="col-md-6">
-                    <select name="institucion" id="institucion" class="form-control" required>
+                  <select class="form-control" name="institucion" id="institucion" required>
                       <option name="institucion" value="" disabled selected>Seleccionar</option>
                       <option name="institucion" value="opcion1">Institución 1</option>
                       <option name="institucion" value="opcion2">Institución 2</option>
@@ -85,34 +95,33 @@
                   </div>
                 </div>
                 
-                <div class="row mb-3">
-                  <label for="programa" class="col-md-4 col-form-label text-md-end">{{ __('Programa educativo *')}}</label>
+                <div class="row justify-content-center mb-3">
+                  <div class="col-md-4">
+                    <label class="control-label" for="programa" class="">{{ __('Programa educativo *')}}</label>
+                  </div>
                   <div class="col-md-6">
-                    <input id="programa" type="text" class="form-control" name="programa" required>
+                    <input class="form-control" id="programa" type="text"  name="programa" required>
                   </div>
                 </div>
                 
-                <div class="row mb-3">
-                  <label for="foto" class="col-md-4 col-form-label text-md-end">{{ __('Fotografia de perfil *')}}</label>
+                <div class="row justify-content-center mb-3">
+                  <div class="col-md-4">
+                    <label for="foto" class="control-label">{{ __('Fotografia de perfil *')}}</label>
+                  </div>  
                   <div class="col-md-6">
-                    <input id="foto" type="file" class="form-control" name="foto" required>
+                    <input class="form-control" id="foto" type="file" name="foto" required>
                   </div>
                 </div>
                 
-                <div class="row mb-0">
-                  <div class="col-md-6 offset-md-4">
+                <div class="row d-flex justify-content-center mb-3">
                     <button type="submit" class="btn btn-primary">
                       {{ __('Enviar') }}
                     </button>
-                  </div>
                 </div>
-                <br>
+                
               </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    </div> 
+    </main>
     @include('layouts/footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-pzjw8N+1lUE3+XjwIXqityaM2UwEJrHjeZX/nezF8+Ad5A0tjFkA5Vx7Js1KrI95" crossorigin="anonymous"></script>
   </body>
