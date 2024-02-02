b1954b2ca491ed87da680b70ddf2dac1340a7673
Barmont1341 <131329899+Barmont1341@users.noreply.github.com>
2024-02-02 11:11:42 -0600

    v2_pdfController_02/02/2024

--- resources/views/pdfPostulacion.blade.php
+++ resources/views/pdfPostulacion.blade.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>PDF Title</title>
 </head>
 <body>
-    <h1>Postulacion PLESITH</h1>
+    <h1>Postulación PLESITH</h1>
     <p>Usuario: {{ $user->name }}</p>
     <p>Email: {{ $user->email }}</p>
-    <p>Email: {{ $user->curp }}</p>
-    <p>Email: {{ $user-> institucion}}</p>
+    <p>CURP: {{ $user->curp }}</p>
+    <p>Institución: {{ $user-> institucion}}</p>
+    <p>
+        <strong>Imagen de Perfil:</strong>
+        <img src="{{ asset('storage/' . $user->image_path) }}" alt="Imagen de Perfil" style="max-width: 200px; max-height: 200px;">
+    </p>
 </body>
 </html>
