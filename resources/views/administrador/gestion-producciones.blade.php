675a4d4d0fc0b38cfffd31f590c8160eed97e9fb
Barmont1341 <131329899+Barmont1341@users.noreply.github.com>
2024-02-29 10:44:38 -0600

    ajuste_gestión_nodos_29/02/2024

--- resources/views/administrador/gestion-producciones.blade.php
+++ resources/views/administrador/gestion-producciones.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('asset/infotech.css') }}">
     <link rel="shortcut icon" type="image/x-icon" href="https://cdn.hidalgo.gob.mx/logo.png"/>
-    <title>Nodos | PLESITH</title>
+    <title>Gestion producciones | PLESITH</title>
 </head>
 <body class="body">
 @include('layouts/headregob')
@@ -14,8 +14,7 @@
         
             <img src="https://lajornadahidalgo.com/wp-content/uploads/2022/08/CITNOVA-SINCROTON.jpg" alt="img">
         <div class="title">
-            <h1>Nodos</h1>
-            
+            <h1>Gestion producciones</h1>
         </div>
         
     </main>
