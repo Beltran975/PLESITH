339f2ec7b814894e3a6be27c8aabd76aed5b072c
Barmont1341 <131329899+Barmont1341@users.noreply.github.com>
2024-03-15 10:14:13 -0600

    cambio_home_15/03/2024

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -287,47 +287,6 @@
                             
                     </div>
 
-                    <!--<div class="card-body collapse" id="produccion">
-                        <div id="tab5" class="tabs-producciones active">
-                            <table class="table contenido produccion">
-                                <thead>
-                                    <tr>
-                                        <th>Tipo:</th>
-                                        <th>Evidencia:</th>
-                                        <th>Autores:</th>
-                                        <th>Titulo:</th>
-                                        <th>Descripción:</th>
-                                        <th>Pais:</th>
-                                        <th>Año:</th>
-                                        <th>Proposito:</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Bases de Datos</td>
-                                        <td>Nacional</td>
-                                        <td>Dr. Ana García</td>
-                                        <td>Ing. Carlos Martínez</td>
-                                        <td>Este estudio examina los desarrollos más recientes en la eficiencia de celdas solares, al combinar los principios de nanotecnología, química y física aplicada. Los autores, con experiencia en sus respectivos campos, exploran la mejora de la conversión fotovoltaica y la reducción de pérdidas energéticas.</td>
-                                        <td>Mexico</td>
-                                        <td>2024</td>
-                                        <td>Creacion</td>
-                                        <td>
-                                            <a href="#" class="btn btn-secondary">
-                                                <i class="bi bi-pencil-square"></i>
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <a href="#" class="btn btn-danger">
-                                                <i class="bi bi-trash3-fill"></i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>-->
-
                 </div>
 
                 <!-- Nodos de colaboración -->
