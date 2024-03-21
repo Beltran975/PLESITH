b6c7c555f50c45c550fe44059d7e2569b2bb4ded
BIXIALJAVI <javisap50@gmail.com>
2024-03-20 21:11:09 -0600

    v1_indexinfotechbibliotech_20032024

--- resources/views/administrador/bibliotech/index.blade.php
+++ resources/views/administrador/bibliotech/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('asset/documentosinfo.css') }}">
+    <link rel="stylesheet" href="{{ asset('asset/createbibliotech.css') }}">
     <link rel="shortcut icon" type="image/x-icon" href="https://cdn.hidalgo.gob.mx/logo.png" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Administrador | PLESITH</title>
@@ -16,50 +16,52 @@
         <div class="row">
             <div class="col-xl-12">
                 <div class="col-sm-4 my1">
-                    <a href="{{ route('bibliotech.create')}}" class="btn btn-primary">
+                    <center><a href="{{ route('bibliotech.create')}}" class="btn btn-primary">
                         <i class="bi bi-plus-circle-fill"></i> Crear
-                    </a>
+                    </a></center>
                 </div>
                 <br>
-                <div class="table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Título:</th>
-                                <th>Fecha:</th>
-                                <th>Documento:</th>
-                                <th>Descripción: </th>
-                                <th>Opciones: </th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @if(count($datos)<=0)
-                            <tr>
-                                <td colspan="8"><h4>"No existen registros"</h4></td>
-                            </tr>
-                            @else
-                            @foreach ($datos as $d)
-                            <tr>
-                                <td>{{ $d->titulo}}</td>
-                                <td>{{ $d->year}}</td>
-                                <td><a href="/bibliotech/{{ $d->documento}}" target="blanck_">{{ $d->documento}}</a></td>
-                                <td>{{ $d->descripcion}}</td>
-                                <td>
-                                    <a href="{{route('bibliotech.edit', $d->id)}}" class="btn btn-warning">Editar</a>
-                                </td>
-                                <td>
-                                    <!-- Button delete modal -->
-                                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-delete-{{ $d->id}}">
-                                        Eliminar
-                                    </button>
-                                </td>
-                            </tr>
-                            @include('administrador.bibliotech.delete')
-                            @endforeach
-                            @endif
-                        </tbody>
-                    </table>
+                <div class="col-sm-8 table-wrapper">
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Título:</th>
+                                    <th>Fecha:</th>
+                                    <th>Documento:</th>
+                                    <th>Descripción: </th>
+                                    <th>Opciones: </th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if(count($datos)<=0)
+                                <tr>
+                                    <td colspan="8"><h4>"No existen registros"</h4></td>
+                                </tr>
+                                @else
+                                @foreach ($datos as $d)
+                                <tr>
+                                    <td>{{ $d->titulo}}</td>
+                                    <td>{{ $d->year}}</td>
+                                    <td><a href="/bibliotech/{{ $d->documento}}" target="blanck_">{{ $d->documento}}</a></td>
+                                    <td>{{ $d->descripcion}}</td>
+                                    <td>
+                                        <a href="{{route('bibliotech.edit', $d->id)}}" class="btn btn-warning">Editar</a>
+                                    </td>
+                                    <td>
+                                        <!-- Button delete modal -->
+                                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-delete-{{ $d->id}}">
+                                            Eliminar
+                                        </button>
+                                    </td>
+                                </tr>
+                                @include('administrador.bibliotech.delete')
+                                @endforeach
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/administrador/infotech/index.blade.php
+++ resources/views/administrador/infotech/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('asset/documentosinfo.css') }}">
+    <link rel="stylesheet" href="{{ asset('asset/createinfotech.css') }}">
     <link rel="shortcut icon" type="image/x-icon" href="https://cdn.hidalgo.gob.mx/logo.png" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Administrador | PLESITH</title>
@@ -16,11 +16,12 @@
         <div class="row">
             <div class="col-xl-12">
                 <div class="col-sm-4 my1">
-                    <a href="{{ route('nuevo.create')}}" class="btn btn-primary">
+                    <center><a href="{{ route('nuevo.create')}}" class="btn btn-primary">
                         <i class="bi bi-plus-circle-fill"></i> Crear
-                    </a>
+                    </a></center>
                 </div>
                 <br>
+                <div class="col-sm-8 table-wrapper">
                 <div class="table-responsive">
                     <table class="table table-striped">
                         <thead>
@@ -64,6 +65,7 @@
             </div>
         </div>
     </div>
+</div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
 </html>
